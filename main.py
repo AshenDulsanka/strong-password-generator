3b03a74cc5bbec84bac75f143a61ce6d104d76e9
Ashen Abeysekara <ashendul@gmail.com>
2024-07-02 10:38:34 +0530

    fixed value error not working

--- main.py
+++ main.py
@@ -13,41 +13,44 @@ def new_rand():
             pw_length = int(num_box.get())
             break
         except ValueError:
-            pw_box.insert(0, "Please enter a valid number")
-        
+            pw_box.delete(0, END)
+            pw_box.insert(0, "Please enter a valid choice")
+            return
+
+    pw_box.delete(0, END)
+    pw = ''
+
     while True:
         try:
             if upper.get() == 1 and numbs.get() == 0 and symbs.get() == 0:
-                pw = ''.join(choice(string.ascii_uppercase) for _ in range(pw_length))
+                for x in range(pw_length):
+                    pw += ''.join(choice(string.ascii_uppercase))
                 break
             elif upper.get() == 1 and numbs.get() == 1 and symbs.get() == 0:
-                pw = ''.join(choice(string.ascii_uppercase + string.digits) for _ in range(pw_length))
+                pw += ''.join(choice(string.ascii_uppercase + string.digits) for _ in range(pw_length))
                 break
             elif upper.get() == 1 and numbs.get() == 1 and symbs.get() == 1:
-                pw = ''.join(choice(string.ascii_uppercase + string.digits + string.punctuation) for _ in range(pw_length))
+                pw += ''.join(choice(string.ascii_uppercase + string.digits + string.punctuation) for _ in range(pw_length))
                 break
             elif upper.get() == 0 and numbs.get() == 1 and symbs.get() == 0:
-                pw = ''.join(choice(string.digits) for _ in range(pw_length))
+                pw += ''.join(choice(string.digits) for _ in range(pw_length))
                 break
             elif upper.get() == 0 and numbs.get() == 1 and symbs.get() == 1:
-                pw = ''.join(choice(string.digits + string.punctuation) for _ in range(pw_length))
+                pw += ''.join(choice(string.digits + string.punctuation) for _ in range(pw_length))
                 break
             elif upper.get() == 0 and numbs.get() == 0 and symbs.get() == 1:
-                pw = ''.join(choice(string.punctuation) for _ in range(pw_length))
+                pw += ''.join(choice(string.punctuation) for _ in range(pw_length))
                 break
             elif upper.get() == 1 and numbs.get() == 0 and symbs.get() == 1:
-                pw = ''.join(choice(string.ascii_uppercase + string.punctuation) for _ in range(pw_length))
+                pw += ''.join(choice(string.ascii_uppercase + string.punctuation) for _ in range(pw_length))
                 break
             elif upper.get() == 0 and numbs.get() == 0 and symbs.get() == 0:
-                pw = ''.join(choice(string.ascii_letters) for _ in range(pw_length))
+                pw += ''.join(choice(string.ascii_letters) for _ in range(pw_length))
                 break
             else:
                 pw_box.insert(0, "Please enter a valid choice")
         except ValueError:
             pw_box.insert(0, "Please enter a valid choice")
-
-    pw_box.delete(0, END)
-    pw = ''
 
     pw_box.insert(0, pw)
 
@@ -58,7 +61,7 @@ def copy_clip():
 lf = LabelFrame(window, text="Enter the length of the password: ")
 lf.pack(pady=20)
 
-num_box = Entry(lf, font=("Poppins, 16"))
+num_box = Entry(lf, font=("Poppins, 16"), width=25, justify=CENTER)
 num_box.pack(padx=30, pady=20)
 
 upper = tk.IntVar()
@@ -83,35 +86,7 @@ gen_button.grid(row=0, column=0, padx=10)
 clip_button = Button(button_frame, text="Copy to Clipboard", command=copy_clip)
 clip_button.grid(row=0, column=1, padx=10)
 
-pw_box = Entry(window, text='', font=("Poppins, 16"), bd=0, bg="systembuttonface")
-pw_box.pack(pady=20)
+pw_box = Entry(window, text='', font=("Poppins, 16"), bd=0, bg="systembuttonface", width=60, justify=CENTER)
+pw_box.pack(pady=20, padx=50)
 
 window.mainloop()
-
-# while True:
-#     try:
-#         length = int(input("Enter the length of the password: "))
-#         break
-#     except ValueError:
-#         print("Please enter a valid number")
-
-# while True:
-#     try:
-#         choice = int(input("\n1. Letters only \n2. Letters and Numbers \n3. Letters, Numbers and Special Characters \n\nEnter your choice: "))
-
-#         if choice == 1:
-#             password = ''.join(random.choice(string.ascii_letters) for _ in range(length))
-#             break
-#         elif choice == 2:
-#             password = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length))
-#             break
-#         elif choice == 3:
-#             password = ''.join(random.choice(string.ascii_letters + string.digits + string.punctuation) for _ in range(length))
-#             break
-#         else:
-#             print("\n\nPlease enter a valid choice")
-        
-#     except ValueError:
-#         print("\n\nPlease enter a valid choice\n")
-    
-# print("\nYour password is: ", password)
